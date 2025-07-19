f9907b0c81bb4936b0043bbd4a79f38c5d7911c8
Ruslan Ismailov <rusishss@mail.ru>
2025-07-19 22:58:44 +0600

    return to helloWorld

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { join } from 'path';
-import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getImage(@Res() res: Response) {
-    const filePath = '/opt/render/project/dist/public/wakeup.png';
-    return res.sendFile(filePath);
+  getHello() {
+    return this.appService.getHello();
   }
 }
